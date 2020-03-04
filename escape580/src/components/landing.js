ffa594ec027e13f4b327d5d169f8f16e07f691be
Mary Halvorsen <mhalvorsen98@gmail.com>
2020-03-04 11:52:28 -0500

    added basic landing page

---
+++ escape580/src/components/landing.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import '../landing.css';
+
+export function Landing(props) {
+ return (
+    <div id="d-landing">
+      <h id="h-landing" >
+            Escape 580
+      </h>
+      <p id="p-landing">
+        (Spoken) Welcome to our game! As a player, you are a student at Hogwarts who is competing in the Tr-Wizard cup for the
+        Goblet of Fire. You are about to enter the maze, where you will use your controller to navigate through
+        the obstacles of the maze.
+      </p>
+    </div>
+ );
+}
