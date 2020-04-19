7185696b53835c1e463c5a9e31208579127e3a9a
Mary Halvorsen <mhalvorsen98@gmail.com>
2020-04-19 12:40:36 -0400

    chamber comp options and speech sound

--- escape580/src/components/chamber.js
+++ escape580/src/components/chamber.js
@@ -1,9 +1,155 @@
 import React, {useLayoutEffect, useCallback, useState} from 'react';
+import Sound from 'react-sound';
+import '../levels.css'
+import chamber_intro from '../audio/chamber/chamber_intro.m4a';
+import sort_hat_1 from '../audio/chamber/sorting_hat_1.m4a';
+import sort_hat_2 from '../audio/chamber/sorting_hat_2.m4a';
+import sword from '../audio/chamber/sword.m4a';
+import throw_hat from '../audio/chamber/throw_hat.m4a';
+import hat_reply from '../audio/chamber/hat_reply.m4a';
+import powers_reply from '../audio/chamber/powers_reply.m4a';
+import sword_reply from '../audio/chamber/sword_reply.m4a';
+import gryffindor_reply from '../audio/chamber/gryffindor_reply.m4a';
+import snake_hiss from '../audio/chamber/snakehiss2.mp3';
+
+function RenderReply(props) {
+    return (
+        <React.Fragment>
+             <Sound
+               url={props.reply}
+               playStatus={Sound.status.PLAYING}
+               autoLoad={true}
+               loop={false}
+               volume={100}
+              />
+        </React.Fragment>
+    );
+}
 
 export function Chamber(props) {
 
-const handleViewChange = props.handleViewChange === undefined ? null : props.handleViewChange;
+    const handleViewChange = props.handleViewChange === undefined ? null : props.handleViewChange;
 
+    const intro = <Sound
+                        url={chamber_intro}
+                        playStatus={Sound.status.PLAYING}
+                        autoLoad={true}
+                        loop={false}
+                        volume={100}
+                                />;
+
+    const[sound, setSound] = useState(intro);
+
+      const sword_comp = <React.Fragment>
+                                      <Sound
+                                         url={sword}
+                                         playStatus={Sound.status.PLAYING}
+                                         autoLoad={true}
+                                         loop={false}
+                                         volume={75} />
+                                       </React.Fragment>;
+
+        const sort_hat_1_comp =   <React.Fragment>
+                                     <Sound
+                                        url={sort_hat_1}
+                                        playStatus={Sound.status.PLAYING}
+                                        autoLoad={true}
+                                        loop={false}
+                                        volume={100} />
+
+                                      </React.Fragment>;
+
+        const sort_hat_2_comp = <React.Fragment>
+                           <Sound
+                              url={sort_hat_2}
+                              playStatus={Sound.status.PLAYING}
+                              autoLoad={true}
+                              loop={false}
+                              volume={100} />
+                         </React.Fragment>;
+
+        const throw_hat_comp = <React.Fragment>
+                            <Sound
+                               url={throw_hat}
+                               playStatus={Sound.status.PLAYING}
+                               autoLoad={true}
+                               loop={false}
+                               volume={100} />
+
+                        </React.Fragment>;
+
+  const finishedLevel = () => {
+            handleViewChange('level-three');
+        }
+
+    const handleSword = () => {
+       setSound(<Sound
+                  url={sword_reply}
+                  playStatus={Sound.status.PLAYING}
+                  autoLoad={true}
+                  loop={false}
+                  volume={100}
+                  onFinishedPlaying={finishedLevel}
+                />);
+    }
+
+    const handleHat= () => {
+       setSound(<RenderReply reply={powers_reply}/>);
+    }
+
+    const handleBasilisk = () => {
+        setSound(<React.Fragment>
+        <Sound
+           url={hat_reply}
+           playStatus={Sound.status.PLAYING}
+           autoLoad={true}
+           loop={false}
+           volume={100} />
+        <Sound
+           url={snake_hiss}
+           playStatus={Sound.status.PLAYING}
+           autoLoad={true}
+           loop={false}
+           volume={75} />
+        </React.Fragment>);
+    }
+
+    const handleGryffindor = () => {
+         setSound(<RenderReply reply={gryffindor_reply}/>)
+    }
+
+const handleSpace = () => {
+       let id =  console.log(document.activeElement.id);
+       switch(id) {
+       case "hat":
+       handleHat();
+       break;
+       case "sword":
+       handleSword();
+       break;
+       case "basilisk":
+       handleBasilisk();
+       break;
+       case "gryffindor":
+       handleGryffindor();
+       break; }}
+
+ const handleSelect = (index) => {
+        switch(index) {
+            case -1:
+            setSound(sort_hat_1_comp);
+            break;
+            case 1:
+            setSound(sword_comp);
+            break;
+            case 2:
+            setSound(throw_hat_comp);
+            break;
+            case 3:
+            setSound(sort_hat_2_comp);
+            break;
+        }
+    }
 
  const handleKey = useCallback((e) => {
            var event = window.event ? window.event : e;
@@ -16,6 +162,14 @@ const handleViewChange = props.handleViewChange === undefined ? null : props.han
            case "ArrowLeft":
            handleViewChange('level-one');
            break;
+           case "Tab":
+           handleSelect(document.activeElement.tabIndex)
+           break;
+           case " ":
+           handleSpace();
+           break;
+           case "Escape":
+           props.exit();
        }
       }, [props]);
 
@@ -26,7 +180,6 @@ const handleViewChange = props.handleViewChange === undefined ? null : props.han
              };
        });
 
-
     return (
         <div id="chamber-image">
            <div id="d-landing">
@@ -36,22 +189,22 @@ const handleViewChange = props.handleViewChange === undefined ? null : props.han
                           <p id="p-landing">
                             You have entered the chamber of secrets. The monstrous basilisk lurks in the shadows
                              of the chamber, and if you look the basilisk in the eye, you will be frozen and unable
-                             to move! Your job is to defeat the basilisk. Wow! Fawkes the Phoenix has flown into the
+                             to move! Your job is to defeat the basilisk. Fawkes the Phoenix has flown into the
                              chamber and has now blinded the basilisk so that you no longer have to worry about being
                              frozen! You spot the sorting hat on the ground nearby… what do you do next?
                           </p>
                           <div id="d-options>">
                           <p>
-                                                      Your options are:
-                                                      <ul id="devils-ul">
-                                                      <li>A. Put on the sorting hat and hope that it provides you with magical powers!  </li>
-                                                      <li>S. Look inside the sorting hat… there’s something shiny in there that might help. </li>
-                                                      <li>D. Throw the sorting hat at the basilisk to distract it so that you can run out of the chamber. </li>
-                                                      <li>F. Put on the sorting hat and the power of gryffindor will help you defeat the basilisk.</li>
-                                                      </ul>
-                                                    </p>
+                          <div id="d-options">
+                          <button id="hat" tabIndex = "1" onClick={handleHat}> Put on the sorting hat and hope that it provides you with magical powers!</button>
+                          <button tabIndex="2" id="sword" onClick={handleSword}> Look inside the sorting hat… there’s something shiny in there that might help.</button>
+                          <button tabIndex="3" id="basilisk" onClick={handleBasilisk}> Throw the sorting hat at the basilisk to distract it so that you can run out of the chamber.</button>
+                          <button tabIndex="4" id="gryffindor" onClick={handleGryffindor}> Put on the sorting hat and the power of gryffindor will help you defeat the basilisk.</button>
+                          </div>
+                          </p>
                           </div>
                         </div>
+                        {sound}
         </div>
         );
 }
